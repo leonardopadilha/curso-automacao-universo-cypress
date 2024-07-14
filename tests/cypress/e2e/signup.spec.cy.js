3585668a7e2c69620e06e63df2ab0e12015c3499
leonardopadilha <leonardopadilha28@gmail.com>
2024-07-14 11:01:51 -0300

    Separando teste de login conforme contexto

--- tests/cypress/e2e/signup.spec.cy.js
+++ tests/cypress/e2e/signup.spec.cy.js
@@ -4,52 +4,82 @@
 
 describe('Cadastro de usuário', () => {
 
-    const user = {
-        name: "Leonardo Padilha",
-        email: "leonardo@samuraibs.com",
-        password: "pwd123"
-    }
+    context('quando o usuário é novato', function() {
+        const user = {
+            name: "Leonardo Padilha",
+            email: "leonardo@samuraibs.com",
+            password: "pwd123"
+        }
 
-    it('deve cadastrar um novo usuário', function () {        
-        const msg = "Agora você se tornou um(a) Samurai, faça seu login para ver seus agendamentos!"
+        before(function() {
+            cy.task('removeUser', user.email)
+            .then(function(result) {
+                console.log(result)
+            })
+        })
 
-        cy.task('removeUser', user.email)
+        it('deve cadastrar com sucesso', function () {        
+            const msg = "Agora você se tornou um(a) Samurai, faça seu login para ver seus agendamentos!"
+    
+            cy.visit('/signup')
+            cy.get('[placeholder="Nome"]').type(user.name)
+            cy.get('[placeholder="E-mail"]').type(user.email)
+            cy.get('[placeholder="Senha"]').type(user.password)
+    
+            cy.contains('button', 'Cadastrar').click()
+    
+            cy.get('.toast')
+                .should('be.visible')
+                .find('p')
+                .should('have.text', msg)
+    /* 
+            cy.wait(5000)
+            cy.get('body') */
+        })
+    })
+
+    context('quando o email já existe', function() {
+        const userCadastrado = {
+            name: 'João Lucas',
+            email: 'joao@samuraibs.com',
+            password: 'pwd123',
+            is_provider:true
+        }
+
+        before(function() {
+            cy.task('removeUser', userCadastrado.email)
             .then(function(result) {
                 console.log(result)
             })
 
-        cy.visit('/signup')
-        cy.get('[placeholder="Nome"]').type(user.name)
-        cy.get('[placeholder="E-mail"]').type(user.email)
-        cy.get('[placeholder="Senha"]').type(user.password)
+            cy.request(
+                'POST',
+                'http://localhost:3333/users',
+                userCadastrado,
+                //failOnStatusCode: false
+            ).then(function(response) {
+                expect(response.status).to.eq(200)
+            })
+        })
 
-        cy.contains('button', 'Cadastrar').click()
+        it('não deve cadastrar o usuário', function () {
 
-        cy.get('.toast')
-            .should('be.visible')
-            .find('p')
-            .should('have.text', msg)
-/* 
-        cy.wait(5000)
-        cy.get('body') */
-    })
-
-    it('deve exibir email já cadastrado', function () {
-        const msg = "Email já cadastrado para outro usuário."
-
-        cy.visit('/signup')
-        cy.get('[placeholder="Nome"]').type(user.name)
-        cy.get('[placeholder="E-mail"]').type(user.email)
-        cy.get('[placeholder="Senha"]').type(user.password)
-
-        cy.contains('button', 'Cadastrar').click()
-
-        cy.get('.toast')
-            .should('be.visible')
-            .find('p')
-            .should('have.text', msg)
-/* 
-        cy.wait(5000)
-        cy.get('body') */
+            const msg = "Email já cadastrado para outro usuário."
+    
+            cy.visit('/signup')
+            cy.get('[placeholder="Nome"]').type(userCadastrado.name)
+            cy.get('[placeholder="E-mail"]').type(userCadastrado.email)
+            cy.get('[placeholder="Senha"]').type(userCadastrado.password)
+    
+            cy.contains('button', 'Cadastrar').click()
+    
+            cy.get('.toast')
+                .should('be.visible')
+                .find('p')
+                .should('have.text', msg)
+    /* 
+            cy.wait(5000)
+            cy.get('body') */
+        })
     })
 })
