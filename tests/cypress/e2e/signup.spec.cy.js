155b6ea76e8058e88510edda725cb232abaa1e5a
leonardopadilha <leonardopadilha28@gmail.com>
2024-07-18 09:04:26 -0300

    Contexto - Email já existe

--- tests/cypress/e2e/signup.spec.cy.js
+++ tests/cypress/e2e/signup.spec.cy.js
@@ -31,4 +31,38 @@ describe('Cadastro de usuário', () => {
             cy.get('body') */
         })
     })
+
+    context('quando o email já existe', function() {
+        const userCadastrado = {
+            name: 'João Lucas',
+            email: 'joao@samuraibs.com',
+            password: 'pwd123',
+            is_provider:true
+        }
+
+        before(function() {
+            cy.task('removeUser', userCadastrado.email)
+            .then(function(result) {
+                console.log(result)
+            })
+
+            cy.request(
+                'POST',
+                'http://localhost:3333/users',
+                userCadastrado,
+                //failOnStatusCode: false
+            ).then(function(response) {
+                expect(response.status).to.eq(200)
+            })
+        })
+
+        it('não deve cadastrar o usuário', function () {
+            const expectedText = "Email já cadastrado para outro usuário."
+
+            signupPage.go()
+            signupPage.form(userCadastrado)
+            signupPage.submit()
+            signupPage.toast.shouldHaveText(expectedText)
+        })
+    })
 })
