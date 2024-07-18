9635557e1821e2a55eebd7aad09272af4ef36bf9
leonardopadilha <leonardopadilha28@gmail.com>
2024-07-18 09:03:14 -0300

    Utilizando page objects no cenário de usuário novato

--- tests/cypress/e2e/signup.spec.cy.js
+++ tests/cypress/e2e/signup.spec.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+import signupPage from '../support/pages/signup'
 
 //const { faker } = require('@faker-js/faker');
 
@@ -19,64 +20,12 @@ describe('Cadastro de usuário', () => {
         })
 
         it('deve cadastrar com sucesso', function () {        
-            const msg = "Agora você se tornou um(a) Samurai, faça seu login para ver seus agendamentos!"
-    
-            cy.visit('/signup')
-            cy.get('[placeholder^="Nome"]').type(user.name) // começa com...
-            cy.get('[placeholder$="email"]').type(user.email) // termina com...
-            cy.get('[placeholder*="senha"]').type(user.password) // possui em qualquer parte da frase
-    
-            cy.contains('button', 'Cadastrar').click()
-    
-            cy.get('.toast')
-                .should('be.visible')
-                .find('p')
-                .should('have.text', msg)
-    /* 
-            cy.wait(5000)
-            cy.get('body') */
-        })
-    })
+            const expectedText = "Agora você se tornou um(a) Samurai, faça seu login para ver seus agendamentos!"
 
-    context('quando o email já existe', function() {
-        const userCadastrado = {
-            name: 'João Lucas',
-            email: 'joao@samuraibs.com',
-            password: 'pwd123',
-            is_provider:true
-        }
-
-        before(function() {
-            cy.task('removeUser', userCadastrado.email)
-            .then(function(result) {
-                console.log(result)
-            })
-
-            cy.request(
-                'POST',
-                'http://localhost:3333/users',
-                userCadastrado,
-                //failOnStatusCode: false
-            ).then(function(response) {
-                expect(response.status).to.eq(200)
-            })
-        })
-
-        it('não deve cadastrar o usuário', function () {
-
-            const msg = "Email já cadastrado para outro usuário."
-    
-            cy.visit('/signup')
-            cy.get('[placeholder^="Nome"]').type(userCadastrado.name)
-            cy.get('[placeholder$="email"]').type(userCadastrado.email)
-            cy.get('[placeholder*="senha"]').type(userCadastrado.password)
-    
-            cy.contains('button', 'Cadastrar').click()
-    
-            cy.get('.toast')
-                .should('be.visible')
-                .find('p')
-                .should('have.text', msg)
+            signupPage.go()
+            signupPage.form(user)
+            signupPage.submit()
+            signupPage.toast.shouldHaveText(expectedText)
     /* 
             cy.wait(5000)
             cy.get('body') */
